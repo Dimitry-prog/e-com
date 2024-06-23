90641283e9bd21d213d5ddbad015aaa5c061a346
Dimitry_Mytnikau <kfifa3@gmail.com>
2024-06-23 19:48:15 +0300

    feat:  download

--- app/(protected)/admin/(pages)/products/[productId]/download/route.tsx
+++ app/(protected)/admin/(pages)/products/[productId]/download/route.tsx
@@ -1,6 +1,33 @@
-import { NextRequest } from 'next/server';
+import fs from 'fs/promises';
+import { notFound } from 'next/navigation';
+import { NextRequest, NextResponse } from 'next/server';
+
+import { db } from '@/shared/lib/db';
 
 export const GET = async (
   req: NextRequest,
-  { params: productId }: { params: { productId: string } }
-) => {};
+  { params: { productId } }: { params: { productId: string } }
+) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+    select: {
+      filePath: true,
+      name: true,
+    },
+  });
+
+  if (product == null) return notFound();
+
+  const { size } = await fs.stat(product.filePath);
+  const file = await fs.readFile(product.filePath);
+  const extension = product.filePath.split('.').pop();
+
+  return new NextResponse(file, {
+    headers: {
+      'Content-Disposition': `attachment; filename="${product.name}.${extension}"`,
+      'Content-Length': size.toString(),
+    },
+  });
+};
